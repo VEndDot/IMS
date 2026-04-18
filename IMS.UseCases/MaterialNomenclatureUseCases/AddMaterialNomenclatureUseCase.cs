1ae46f1e3925d80edcf1da9e9e9ad6d9d0c75146
Nikolay <nikolya.lysenko@mail.ru>
2026-04-18 20:40:47 +0500

    Добавил номенклатуру

---
+++ IMS.UseCases/MaterialNomenclatureUseCases/AddMaterialNomenclatureUseCase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMS.UseCases.MaterialNomenclatureUseCases
+{
+    public class AddMaterialNomenclatureUseCase
+    {
+    }
+}
